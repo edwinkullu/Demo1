7cb50ee81cc545647f107afa76e96364c116d434
edwin kullu <edwinkullu94@gmail.com>
2023-06-05 15:42:28 +0530

    update new file

---
+++ script.ps1
@@ -0,0 +1,13 @@
+git init
+
+git config --global user.name "edwin kullu"
+
+git config --global user.email "edwinkullu94@gmail.com"
+
+git add .
+
+git commit -m "update new file"
+
+
+
+git push  origin main
