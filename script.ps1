08cf3d04e14418338650672b08a800ff18a7c5b6
edwin kullu <edwinkullu94@gmail.com>
2023-06-07 15:09:22 +0530

    update new file

--- script.ps1
+++ script.ps1
@@ -13,14 +13,14 @@ git commit -m "update new file"
 
 git push  -uf origin main
 
-#$i = Get-Date -Format "MM.dd.yyyy HH:mm:ss"
+$i = Get-Date -Format "MM.dd.yyyy HH:mm:ss"
 
-
+echo $i
 # Build the Docker image with the updated version number
 
 #docker build --tag webapp1:v1.$i .
 
-docker build . --file Dockerfile --tag webapp:v1.1
+docker build . --file Dockerfile --tag webapp:v1.$i
 
 #docker tag webapp:latest azureregistery000.azurecr.io/webapp:v1.$i
 
