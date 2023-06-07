9bd74cf359bbc299d83fc3d3517d724083e0512f
edwin kullu <edwinkullu94@gmail.com>
2023-06-07 15:27:59 +0530

    update new file

--- script.ps1
+++ script.ps1
@@ -24,7 +24,7 @@ docker build . --file Dockerfile --tag webapp:$i
 
 #docker tag webapp:latest azureregistery000.azurecr.io/webapp:v1.$i
 
-az login --service-principal -u 85f4c690-8f79-48bb-a917-1647925d614e -p Lii8Q~E_ZMxJvOE-JDYhu3NcCGRlfK1Qn8NW7acq --tenant 4ec9e9d4-1dad-427f-adf9-e774dca413d1
+az login --service-principal -u "85f4c690-8f79-48bb-a917-1647925d614e" -p "Lii8Q~E_ZMxJvOE-JDYhu3NcCGRlfK1Qn8NW7acq" --tenant "4ec9e9d4-1dad-427f-adf9-e774dca413d1"
 
 az acr login -n azureregistery000
 
