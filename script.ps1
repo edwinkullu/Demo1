17fb8bceb7bcbe685fe2dc201c148374e53fa276
edwin kullu <edwinkullu94@gmail.com>
2023-06-07 14:57:29 +0530

    update new file

--- script.ps1
+++ script.ps1
@@ -23,14 +23,15 @@ git push  -uf origin main
 docker build . --file Dockerfile --tag webapp:v1.0
 
 #docker tag webapp:latest azureregistery000.azurecr.io/webapp:v1.$i
+$app_id="85f4c690-8f79-48bb-a917-1647925d614e"
+$password="Lii8Q~E_ZMxJvOE-JDYhu3NcCGRlfK1Qn8NW7acq"
+$tenant_id="4ec9e9d4-1dad-427f-adf9-e774dca413d1"
+az login --service-principal -u ${app_id} -p ${password} --tenant ${tenant_id}
+
+az acr login -n azureregistery000
 
 
-#az login 
+docker push webapp:v1.0
 
-#az acr login -n azureregistery000
-
-
-#docker push azureregistery000.azurecr.io/webapp:v1.$version
-
-#az acr repository list --name azureregistery000 --output table
+az acr repository list --name azureregistery000 --output table
 
